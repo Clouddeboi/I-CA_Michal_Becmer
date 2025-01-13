888ee70c3de6ee9a40007d06635e92ecee6a030f
Cloud <michalcloud2003@gmail.com>
2025-01-13 14:03:04 +0000

    ✨ Added end game manager script
    very similar to tutorial manager but doesn't require any input from the user so the code is even simpler.

---
+++ Assets/I-CA_Michal_Becmer/My Assets/Scripts/Managers/EndingVoicelinesManager.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+using UnityEngine.InputSystem;
+
+public class EndingsVoicelinesManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text subtitleText;//Reference to the UI Text for subtitles
+    [SerializeField] private AudioManager audioManager;//Reference to the AudioManager
+
+    [System.Serializable]
+    public class EndGameStep
+    {
+        public AudioClip voiceLine;//Voice line to play
+        public string subtitleText;//Subtitle to display
+        public bool requiresInput;//Bool to check if this step requires input to proceed
+    }
+
+    [Header("Tutorial Steps")]
+    //Array of tutorial steps, each step has a voiceline and subtitles
+    [SerializeField] private EndGameStep[] endGameSteps;
+
+    private int currentStepIndex = 0;
+
+
+    private void Start()
+    {
+        if (endGameSteps.Length > 0)
+        {
+            //Start the EndGame
+            StartCoroutine(PlayEndGame());
+        }
+        else
+        {
+            //Warning if no tutorial steps are set
+            Debug.LogWarning("No End Game steps found!");
+        }
+    }
+
+    private IEnumerator PlayEndGame()
+    {
+        while (currentStepIndex < endGameSteps.Length)
+        {
+            EndGameStep step = endGameSteps[currentStepIndex];
+
+            //Display the subtitle
+            subtitleText.text = step.subtitleText;
+
+            //Play the voice line
+            if (step.voiceLine != null && audioManager != null)
+            {
+                audioManager.PlaySFX(step.voiceLine);
+
+                //Wait until the audio finishes
+                yield return new WaitForSeconds(step.voiceLine.length);
+            }
+            else
+            {
+                //In case we filled in the step incorrectly
+                Debug.LogWarning($"Voice line or AudioManager missing for step {currentStepIndex}");
+                //Default duration
+                yield return new WaitForSeconds(2f);
+            }
+
+            //Clear the subtitle after this step
+            subtitleText.text = "";
+
+            //Wait 1 second before the next step
+            yield return new WaitForSeconds(1f);
+
+            //Move to the next step
+            currentStepIndex++;
+        }
+
+        //Log when the tutorial is completed
+        Debug.Log("Ending finished!");
+    }
+
+}
